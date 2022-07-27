8b96f9cb502b2de2a06123503999b62e4c043570
Jeremy Cook <jeremycook97@gmail.com>
2022-07-26 21:57:59 -0500

    Organized code a little better

--- one-percent-better/one-percent-better/Habit Views/GradientRing.swift
+++ one-percent-better/one-percent-better/Habit Views/GradientRing.swift
@@ -7,9 +7,51 @@
 
 import SwiftUI
 
-struct GradientRing: View, Animatable {
+
+class GradientRingViewModel {
     
-    @Environment(\.colorScheme) var colorScheme
+    /// The color at the start of the gradient
+    var startColor = Color(#colorLiteral(red: 0, green: 0.7286170125, blue: 0.879304111, alpha: 1))
+    
+    /// The color at the end of the gradient
+    var endColor = Color(#colorLiteral(red: 0.009636783041, green: 0.9831244349, blue: 0.8203613162, alpha: 1))
+    
+    /// The diameter of the circle
+    var size: CGFloat = 250
+    
+    /// The line width of the circle
+    var lineWidth: CGFloat = 50
+    
+    /// Opacity of the shadow on ends of the circle
+    var shadowOpacity: Double = 0.15
+    
+    /// Radius of shadow at the ends of the circle
+    var shadowRadius: CGFloat {
+        lineWidth/4
+    }
+    
+    let startAngle: CGFloat = 25
+    let endAngle: CGFloat = 2
+    var angularGradient: AngularGradient {
+        AngularGradient(gradient: Gradient(colors: [endColor, startColor]),
+                        center: .center,
+                        startAngle: .init(degrees: startAngle),
+                        endAngle: .init(degrees: 360 + endAngle))
+    }
+    
+    init(startColor: Color, endColor: Color, size: CGFloat, lineWidth: CGFloat?) {
+        self.startColor = startColor
+        self.endColor = endColor
+        self.size = size
+        if lineWidth != nil {
+            self.lineWidth = lineWidth!
+        } else {
+            self.lineWidth = size / 5
+        }
+    }
+}
+
+struct GradientRing: View, Animatable {
     
     /// The percent completion of the circle
     /// Last point before the two ends touch: 0.935
@@ -40,50 +82,18 @@ struct GradientRing: View, Animatable {
         return result
     }
     
-    /// The color at the start of the gradient
-    var startColor = Color(#colorLiteral(red: 0, green: 0.7286170125, blue: 0.879304111, alpha: 1))
+    var vm: GradientRingViewModel
     
-    /// The color at the end of the gradient
-    var endColor = Color(#colorLiteral(red: 0.009636783041, green: 0.9831244349, blue: 0.8203613162, alpha: 1))
-    
-    /// The diameter of the circle
-    var size: CGFloat = 250
-    
-    /// The line width of the circle
-    var lineWidth: CGFloat = 50
-    
-    /// Opacity of the shadow on ends of the circle
-    var shadowOpacity: Double = 1.0
-    
-    /// Radius of shadow at the ends of the circle
-    var shadowRadius: CGFloat {
-        lineWidth/4
-    }
-    
-    @State private var rotation: CGFloat = 0.0
-    
-    init(percent: Double) {
+    init(percent: Double,
+         startColor: Color = Color(#colorLiteral(red: 0, green: 0.7286170125, blue: 0.879304111, alpha: 1)),
+         endColor: Color = Color(#colorLiteral(red: 0.009636783041, green: 0.9831244349, blue: 0.8203613162, alpha: 1)),
+         size: CGFloat = 250,
+         lineWidth: CGFloat? = nil) {
         self.percent = percent
-        self.lineWidth = size / 5
-        self.shadowOpacity = colorScheme == .light ? 0.1 : 1.0
-    }
-    
-    init(percent: Double, startColor: Color = Color(#colorLiteral(red: 0, green: 0.7286170125, blue: 0.879304111, alpha: 1)), endColor: Color = Color(#colorLiteral(red: 0.009636783041, green: 0.9831244349, blue: 0.8203613162, alpha: 1)), size: CGFloat = 250) {
-        self.percent = percent
-        self.shadowOpacity = colorScheme == .light ? 0.1 : 1.0
-        self.startColor = startColor
-        self.endColor = endColor
-        self.size = size
-        self.lineWidth = size / 5
-    }
-    
-    init(percent: Double, startColor: Color = Color(#colorLiteral(red: 0, green: 0.7286170125, blue: 0.879304111, alpha: 1)), endColor: Color = Color(#colorLiteral(red: 0.009636783041, green: 0.9831244349, blue: 0.8203613162, alpha: 1)), size: CGFloat = 250, lineWidth: CGFloat) {
-        self.percent = percent
-        self.shadowOpacity = colorScheme == .light ? 0.1 : 1.0
-        self.startColor = startColor
-        self.endColor = endColor
-        self.size = size
-        self.lineWidth = lineWidth
+        vm = GradientRingViewModel(startColor: startColor,
+                                   endColor: endColor,
+                                   size: size,
+                                   lineWidth: lineWidth)
     }
     
     var body: some View {
@@ -93,86 +103,27 @@ struct GradientRing: View, Animatable {
             // Circle
             ZStack {
                 
-                GrayCircle(diameter: size, lineWidth: lineWidth)
-                
-                let startAngle: CGFloat = 25
-                let endAngle: CGFloat = 2
-                let angularGradient = AngularGradient(gradient: Gradient(colors: [endColor, startColor]),
-                                                      center: .center,
-                                                      startAngle: .init(degrees: startAngle),
-                                                      endAngle: .init(degrees: 360 + endAngle))
+                GrayCircle(vm: vm)
                 
                 // Start circle
                 if percent > 0 {
-                    ZStack {
-                        ZStack {
-                            Circle()
-                                .trim(from: 0, to: 0.0000000001)
-                                .stroke(startColor, style: .init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
-                                .rotation3DEffect(.init(degrees: 180), axis: (x: 1, y: 0, z: 0))
-                                .rotation3DEffect(.init(degrees: -90), axis: (x: 0, y: 0, z: 1))
-                                .shadow(color: percent > cutoffPercent ? .black.opacity(shadowOpacity) : .black.opacity(0), radius: shadowRadius)
-                            .frame(width: size, height: size)
-                        }
-                        .frame(width: size + lineWidth, height: size + lineWidth)
-                        .reverseMask {
-                            RingCutout(from: 0, to: 0.1, clockwise: true)
-                        }
-                        .clipShape(Circle())
-                    }
-                    .reverseMask {
-                        Circle()
-                            .frame(width: size - lineWidth, height: size - lineWidth)
-                    }
+                    StartCircle(vm: vm,
+                                percent: percent,
+                                cutoffPercent: cutoffPercent)
                 }
 
                 // Main Loop circle
-                ZStack {
-                    ZStack {
-                        Circle()
-                            .trim(from: 1 - percentWithRotationCutoff, to: 1)
-                            .stroke(angularGradient, style: .init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
-                            .rotation3DEffect(.init(degrees: 180), axis: (x: 1, y: 0, z: 0))
-                            .rotation3DEffect(.init(degrees: -90), axis: (x: 0, y: 0, z: 1))
-                            .shadow(color: .black.opacity(shadowOpacity), radius: shadowRadius)
-                            .frame(width: size, height: size)
-                    }
-                    .frame(width: size + lineWidth, height: size + lineWidth)
-                    .reverseMask {
-                        if percent > cutoffPercent {
-                            RingCutout(from: 0, to: cutoffPercent + 0.01, clockwise: false)
-                        }
-                    }
-                    .clipShape(Circle())
-                }
-                .reverseMask {
-                    Circle()
-                        .frame(width: size - lineWidth, height: size - lineWidth)
-                }
+                MainLoopCircle(vm: vm,
+                               percent: percent,
+                               cutoffPercent: cutoffPercent,
+                               percentWithRotationCutoff: percentWithRotationCutoff)
                 
                 
                 // Overlapping circle
                 if percent > cutoffPercent {
-                    ZStack {
-                        ZStack {
-                            let angleOffset: CGFloat = 360 - 360 * cutoffPercent
-                            Circle()
-                                .trim(from: 0, to: continueAngle)
-                                .stroke(endColor, style: .init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
-                                .rotation3DEffect(.init(degrees: -90 - angleOffset), axis: (x: 0, y: 0, z: 1))
-                                .shadow(color: .black.opacity(shadowOpacity), radius: shadowRadius)
-                                .frame(width: size, height: size)
-                        }
-                        .frame(width: size + lineWidth, height: size + lineWidth)
-                        .reverseMask {
-                            RingCutout(from: cutoffPercent, to: cutoffPercent - 0.1, clockwise: false)
-                        }
-                        .clipShape(Circle())
-                    }
-                    .reverseMask {
-                        Circle()
-                            .frame(width: size - lineWidth, height: size - lineWidth)
-                    }
+                    OverlappingCircle(vm: vm,
+                                      cutoffPercent: cutoffPercent,
+                                      continueAngle: continueAngle)
                 }
             }
             .rotationEffect(Angle(degrees: rotationDegrees))
@@ -181,45 +132,115 @@ struct GradientRing: View, Animatable {
 }
 
 struct GradientRing_Previews: PreviewProvider {
-    
-    @State static var percent: Double = 0.3
-    @State static var count: Int = 0
-    
     static var previews: some View {
-        Group {
-            VStack {
-                GradientRing(percent: percent, lineWidth: 50)
-                    .animation(.easeInOut, value: percent)
-                
-                Button("Toggle") {
-                    withAnimation {
-                        percent = percent < 0.5 ? 1.0 : 0.0
-                        count += 1
-                    }
-                }
-                
-                Text("\(count)")
-            }
-            
-            ZStack {
-                Text("Test")
-            }
-        }
+        GradientRing(percent: 0.3, lineWidth: 50)
     }
 }
 
 
 struct GrayCircle: View {
-
-    let diameter: CGFloat
-    let lineWidth: CGFloat
+    
+    let vm: GradientRingViewModel
     
     var body: some View {
         Circle()
             .trim(from: 0, to: 1)
-            .stroke(Color.gray.opacity(0.15), style: .init(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+            .stroke(Color.gray.opacity(0.15), style: .init(lineWidth: vm.lineWidth, lineCap: .round, lineJoin: .round))
             .rotation3DEffect(.init(degrees: 180), axis: (x: 1, y: 0, z: 0))
             .rotation3DEffect(.init(degrees: -90), axis: (x: 0, y: 0, z: 1))
-            .frame(width: diameter, height: diameter)
+            .frame(width: vm.size, height: vm.size)
+    }
+}
+
+struct StartCircle: View {
+    
+    let vm: GradientRingViewModel
+    let percent: Double
+    let cutoffPercent: Double
+    
+    var body: some View {
+        ZStack {
+            ZStack {
+                Circle()
+                    .trim(from: 0, to: 0.0000000001)
+                    .stroke(vm.startColor, style: .init(lineWidth: vm.lineWidth, lineCap: .round, lineJoin: .round))
+                    .rotation3DEffect(.init(degrees: 180), axis: (x: 1, y: 0, z: 0))
+                    .rotation3DEffect(.init(degrees: -90), axis: (x: 0, y: 0, z: 1))
+                    .shadow(color: percent > cutoffPercent ? .black.opacity(vm.shadowOpacity) : .black.opacity(0), radius: vm.shadowRadius)
+                    .frame(width: vm.size, height: vm.size)
+            }
+            .frame(width: vm.size + vm.lineWidth, height: vm.size + vm.lineWidth)
+            .reverseMask {
+                RingCutout(from: 0, to: 0.1, clockwise: true)
+            }
+            .clipShape(Circle())
+        }
+        .reverseMask {
+            Circle()
+                .frame(width: vm.size - vm.lineWidth, height: vm.size - vm.lineWidth)
+        }
+    }
+}
+
+struct MainLoopCircle: View {
+    
+    let vm: GradientRingViewModel
+    let percent: Double
+    let cutoffPercent: Double
+    let percentWithRotationCutoff: Double
+    
+    var body: some View {
+        ZStack {
+            ZStack {
+                Circle()
+                    .trim(from: 1 - percentWithRotationCutoff, to: 1)
+                    .stroke(vm.angularGradient, style: .init(lineWidth: vm.lineWidth, lineCap: .round, lineJoin: .round))
+                    .rotation3DEffect(.init(degrees: 180), axis: (x: 1, y: 0, z: 0))
+                    .rotation3DEffect(.init(degrees: -90), axis: (x: 0, y: 0, z: 1))
+                    .shadow(color: .black.opacity(vm.shadowOpacity), radius: vm.shadowRadius)
+                    .frame(width: vm.size, height: vm.size)
+            }
+            .frame(width: vm.size + vm.lineWidth, height: vm.size + vm.lineWidth)
+            .reverseMask {
+                if percent > cutoffPercent {
+                    RingCutout(from: 0, to: cutoffPercent + 0.01, clockwise: false)
+                }
+            }
+            .clipShape(Circle())
+        }
+        .reverseMask {
+            Circle()
+                .frame(width: vm.size - vm.lineWidth, height: vm.size - vm.lineWidth)
+        }
+    }
+}
+
+struct OverlappingCircle: View {
+    
+    var vm: GradientRingViewModel
+    let cutoffPercent: Double
+    let continueAngle: CGFloat
+    
+    var body: some View {
+        ZStack {
+            ZStack {
+                let angleOffset: CGFloat = 360 - 360 * cutoffPercent
+                Circle()
+                    .trim(from: 0, to: continueAngle)
+                    .stroke(vm.endColor, style: .init(lineWidth: vm.lineWidth, lineCap: .round, lineJoin: .round))
+                    .rotation3DEffect(.init(degrees: -90 - angleOffset), axis: (x: 0, y: 0, z: 1))
+                    .shadow(color: .black.opacity(vm.shadowOpacity), radius: vm.shadowRadius)
+                    .frame(width: vm.size, height: vm.size)
+            }
+            .frame(width: vm.size + vm.lineWidth, height: vm.size + vm.lineWidth)
+            .reverseMask {
+                RingCutout(from: cutoffPercent, to: cutoffPercent - 0.1, clockwise: false)
+            }
+            .clipShape(Circle())
+        }
+        .reverseMask {
+            Circle()
+                .frame(width: vm.size - vm.lineWidth, height: vm.size - vm.lineWidth)
+        }
     }
 }
