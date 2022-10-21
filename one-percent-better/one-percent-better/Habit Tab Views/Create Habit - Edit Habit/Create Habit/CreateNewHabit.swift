b4709a2a6af99f54da8f929c191c69f43dbfb797
Jeremy Cook <jeremycook97@gmail.com>
2022-10-21 14:23:09 -0400

    Squashed all bugs

--- one-percent-better/one-percent-better/Habit Tab Views/Create Habit - Edit Habit/Create Habit/CreateNewHabit.swift
+++ one-percent-better/one-percent-better/Habit Tab Views/Create Habit - Edit Habit/Create Habit/CreateNewHabit.swift
@@ -9,74 +9,74 @@ import SwiftUI
 import Introspect
 
 enum CreateFrequencyRoute: Hashable {
-  case createFrequency(String)
+   case createFrequency(String)
 }
 
 struct CreateNewHabit: View {
-  
-  @EnvironmentObject var nav: HabitTabNavPath
-  
-  @EnvironmentObject var vm: HabitListViewModel
-  
-  @State var habitName: String = ""
-  @State private var isResponder: Bool? = true
-  
-  @State private var nextView = false
-  
-  var body: some View {
-    Background {
-      VStack {
-        HabitCreationHeader(systemImage: "square.and.pencil",
-                            title: "Create New Habit")
-        
-        VStack {
-          ZStack {
-            RoundedRectangle(cornerRadius: 10)
-              .foregroundColor(.cardColor)
-              .frame(height: 50)
+   
+   @EnvironmentObject var nav: HabitTabNavPath
+   
+   @EnvironmentObject var vm: HabitListViewModel
+   
+   @State var habitName: String = ""
+   @State private var isResponder: Bool? = true
+   
+   @State private var nextView = false
+   
+   var body: some View {
+      Background {
+         VStack {
+            HabitCreationHeader(systemImage: "square.and.pencil",
+                                title: "Create New Habit")
             
-            CustomTextField(text: $habitName,
-                            placeholder: "Name",
-                            isResponder: $isResponder,
-                            nextResponder: .constant(nil))
-            .padding(.leading, 10)
-            .frame(height: 50)
-          }
-          .padding(.horizontal, 20)
-        }
-        
-        Spacer()
-        
-        BottomButtonDisabledWhenEmpty(text: "Next", dependingLabel: $habitName)
-          .onTapGesture {
-            if !habitName.isEmpty {
-              nav.path.append(CreateFrequencyRoute.createFrequency(habitName))
+            VStack {
+               ZStack {
+                  RoundedRectangle(cornerRadius: 10)
+                     .foregroundColor(.cardColor)
+                     .frame(height: 50)
+                  
+                  CustomTextField(text: $habitName,
+                                  placeholder: "Name",
+                                  isResponder: $isResponder,
+                                  nextResponder: .constant(nil))
+                  .padding(.leading, 10)
+                  .frame(height: 50)
+               }
+               .padding(.horizontal, 20)
             }
-          }
-          .navigationDestination(for: CreateFrequencyRoute.self) { route in
-            if case let .createFrequency(habitName) = route {
-              ChooseHabitFrequency(habitName: habitName)
-                .toolbar(.hidden, for: .tabBar)
-                .environmentObject(vm)
-            }
-          }
+            
+            Spacer()
+            
+            BottomButtonDisabledWhenEmpty(text: "Next", dependingLabel: $habitName)
+               .onTapGesture {
+                  if !habitName.isEmpty {
+                     nav.path.append(CreateFrequencyRoute.createFrequency(habitName))
+                  }
+               }
+               .navigationDestination(for: CreateFrequencyRoute.self) { route in
+                  if case let .createFrequency(habitName) = route {
+                     ChooseHabitFrequency(habitName: habitName)
+                        .toolbar(.hidden, for: .tabBar)
+                        .environmentObject(vm)
+                  }
+               }
+         }
       }
-    }
-    .toolbar {
-      ToolbarItem(placement: .principal) {
-        // this sets the screen title in the navigation bar, when the screen is visible
-        Text("")
+      .toolbar {
+         ToolbarItem(placement: .principal) {
+            // This sets the back button as "Back" instead of the title of the previous screen
+            Text("    ")
+         }
       }
-    }
-    .toolbar(.hidden, for: .tabBar)
-  }
+      .toolbar(.hidden, for: .tabBar)
+   }
 }
 
 struct CreateNewHabit_Previews: PreviewProvider {
-  
-  static var previews: some View {
-    let context = CoreDataManager.previews.mainContext
-    CreateNewHabit()
-      .environment(\.managedObjectContext, context)
-  }
+   
+   static var previews: some View {
+      let context = CoreDataManager.previews.mainContext
+      CreateNewHabit()
+         .environment(\.managedObjectContext, context)
+   }
 }
--- one-percent-better/one-percent-better/Habit Tab Views/Habit List Views/Enter Data Views/DeletableRow.swift
+++ one-percent-better/one-percent-better/Habit Tab Views/Habit List Views/Enter Data Views/DeletableRow.swift
@@ -8,123 +8,133 @@
 import SwiftUI
 
 struct DeletableRow<Content>: View where Content: View {
-    
-    @State private var offset = CGSize.zero
-    @State private var isDragging: Bool = false
-    
-    @State private var rightMenuOffset: CGFloat = 0
-    
-    var rightMenuOffsetPositive: CGFloat {
-        max(0, min(rightMenuOffset, 200))
-    }
-    
-    @State private var rightMenuOpen = false
-    var rightMenuWidth: CGFloat = 70
-    
-    var size: CGFloat = 30
-    
-    @State private var contentHeight: CGFloat = 0
-    @State private var contentWidth: CGFloat = 0
-    
-    let content: () -> Content
-    
-    let deleteCallback: () -> Void
-    
-    var body: some View {
-        
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
-                if !rightMenuOpen {
-                    rightMenuOffset = -offset.width
-                } else {
-                    rightMenuOffset = rightMenuWidth - offset.width
-                }
+   
+   @State private var offset = CGSize.zero
+   
+   @State private var rightMenuOffset: CGFloat = 0
+   
+   var rightMenuOffsetPositive: CGFloat {
+      max(0, min(rightMenuOffset, 200))
+   }
+   
+   @State private var rightMenuOpen = false
+   var rightMenuWidth: CGFloat = 70
+   
+   var size: CGFloat = 30
+   
+   @State private var contentHeight: CGFloat = 0
+   @State private var contentWidth: CGFloat = 0
+   
+   let content: () -> Content
+   
+   let deleteCallback: () -> Void
+   
+   func onEndedDrag() {
+      withAnimation {
+         if -offset.width > 50 {
+            rightMenuOpen = true
+            rightMenuOffset = rightMenuWidth
+         } else {
+            rightMenuOpen = false
+            rightMenuOffset = 0
+            offset = .zero
+         }
+      }
+   }
+   
+   var body: some View {
+      
+      let dragGesture = DragGesture()
+         .onChanged { value in
+            offset = value.translation
+            if !rightMenuOpen {
+               rightMenuOffset = -offset.width
+            } else {
+               rightMenuOffset = rightMenuWidth - offset.width
             }
-            .onEnded { _ in
-                withAnimation {
-                    if -offset.width > 50 {
-                        rightMenuOpen = true
-                        rightMenuOffset = rightMenuWidth
-                    } else {
-                        rightMenuOpen = false
-                        rightMenuOffset = 0
-                        offset = .zero
-                    }
-                }
-                isDragging = false
-            }
-        
-        ZStack {
-            HStack {
-                content()
-                    .offset(x: -rightMenuOffsetPositive)
-                    .overlay(
-                        GeometryReader { geo in
-                            Color.clear.onAppear {
-                                self.contentHeight = geo.size.height
-                            }
-                        }
-                    )
-            }
-            .background(Color.cardColor)
-            .gesture(dragGesture)
-            
-            HStack {
-                Spacer()
-                Rectangle()
-                    .fill(.red)
-                    .frame(width: rightMenuOffsetPositive)
-            }
-            
-            let trashWidth: CGFloat = min(23, contentHeight/2)
-            let rawTrashOffset = -rightMenuOffset + rightMenuWidth/2 + trashWidth/2
-            let maxTrashOffset = -rightMenuWidth + rightMenuWidth/2 + trashWidth/2
-            let trashOffset = rawTrashOffset < maxTrashOffset ? maxTrashOffset : rawTrashOffset
-            
-            HStack {
-                Spacer()
-                Image(systemName: "trash.fill")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                .frame(width: trashWidth)
-                .offset(x: trashOffset)
-                .foregroundColor(.white)
-                .onTapGesture {
-                    withAnimation {
-                        deleteCallback()
-                        rightMenuOpen = false
-                        rightMenuOffset = 0
-                        offset = .zero
-                    }
-                }
-            }
-        }
-        .clipped()
-        .frame(height: contentHeight)
-    }
+         }
+         .onEnded { _ in
+            onEndedDrag()
+         }
+      
+      ZStack {
+         HStack {
+            content()
+               .offset(x: -rightMenuOffsetPositive)
+               .overlay(
+                  GeometryReader { geo in
+                     Color.clear.onAppear {
+                        self.contentHeight = geo.size.height
+                     }
+                  }
+               )
+         }
+         .background(Color.cardColor)
+         .simultaneousGesture(dragGesture)
+         
+         HStack {
+            Spacer()
+            Rectangle()
+               .fill(.red)
+               .frame(width: rightMenuOffsetPositive)
+         }
+         
+         let trashWidth: CGFloat = min(23, contentHeight/2)
+         let rawTrashOffset = -rightMenuOffset + rightMenuWidth/2 + trashWidth/2
+         let maxTrashOffset = -rightMenuWidth + rightMenuWidth/2 + trashWidth/2
+         let trashOffset = rawTrashOffset < maxTrashOffset ? maxTrashOffset : rawTrashOffset
+         
+         HStack {
+            Spacer()
+            Image(systemName: "trash.fill")
+               .resizable()
+               .aspectRatio(contentMode: .fit)
+               .frame(width: trashWidth)
+               .offset(x: trashOffset)
+               .foregroundColor(.white)
+               .onTapGesture {
+                  withAnimation {
+                     deleteCallback()
+                     rightMenuOpen = false
+                     rightMenuOffset = 0
+                     offset = .zero
+                  }
+               }
+         }
+      }
+      .clipped()
+      .frame(height: contentHeight)
+      .background(Group{
+          GeometryReader{ geo in
+              HStack {}
+              .onChange(of: geo.frame(in: .global).minY) { _ in
+                 onEndedDrag()
+              }
+          }
+      })
+   }
 }
 
 struct DeletableRowPreview: View {
-    
-    @State private var rowText: String = "Hello World!"
-    
-    var body: some View {
-        Background {
-            DeletableRow {
-                Text(rowText)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 30)
-            } deleteCallback: {
-                rowText = "Deleted"
-            }
-        }
-    }
+   
+   @State private var rowText: String = "Hello World!"
+   
+   var body: some View {
+      Background {
+         DeletableRow {
+            Text(rowText)
+               .frame(maxWidth: .infinity)
+               .frame(height: 30)
+         } deleteCallback: {
+            rowText = "Deleted"
+         }
+      }
+   }
 }
 
 
 struct DeletableRow_Previews: PreviewProvider {
-    static var previews: some View {
-        DeletableRowPreview()
-    }
+   static var previews: some View {
+      DeletableRowPreview()
+   }
 }
