efddc04c15b046b1e7b034d8fd734e2cf68ec843
Jeremy Cook <jeremycook97@gmail.com>
2022-12-22 16:33:10 -0600

    Remove pods

--- one-percent-better/Pods/Target Support Files/Pods-one-percent-better-one-percent-betterUITests/Pods-one-percent-better-one-percent-betterUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_one_percent_better_one_percent_betterUITests : NSObject
-@end
-@implementation PodsDummy_Pods_one_percent_better_one_percent_betterUITests
-@end
