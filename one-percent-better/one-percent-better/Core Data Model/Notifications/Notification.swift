c58ba7fcb652f4cfab1dc250931fb67630298630
Jeremy Cook <jeremycook97@gmail.com>
2023-08-15 20:34:56 -0500

    Fixed notification crashes

--- one-percent-better/one-percent-better/Core Data Model/Notifications/Notification.swift
+++ one-percent-better/one-percent-better/Core Data Model/Notifications/Notification.swift
@@ -35,15 +35,25 @@ public class Notification: NSManagedObject {
         if unscheduledNotificationStrings.isEmpty {
             let messages = try await notificationGenerator.generateNotifications(habitName: habit.name)
             await moc.perform {
+                guard !self.isDeleted && self.managedObjectContext != nil else {
+                    NotificationManager.shared.rebalanceTask?.cancel()
+                    return
+                }
                 self.unscheduledNotificationStrings = messages
             }
+            try Task.checkCancellation()
         }
         var message: String!
         await moc.perform {
+            guard !self.isDeleted && self.managedObjectContext != nil else {
+                NotificationManager.shared.rebalanceTask?.cancel()
+                return
+            }
             message = self.unscheduledNotificationStrings.removeLast()
             let scheduledNotification = ScheduledNotification(context: self.moc, index: index, date: date, string: message, notification: self)
             self.addToScheduledNotifications(scheduledNotification)
         }
+        try Task.checkCancellation()
         print("Adding to scheduled notification for id: \(self.id), index: \(index), date: \(date)")
         return message
     }
--- one-percent-better/one-percent-better/Core Data Model/Notifications/RandomTimeNotification.swift
+++ one-percent-better/one-percent-better/Core Data Model/Notifications/RandomTimeNotification.swift
@@ -11,74 +11,78 @@ import CoreData
 
 @objc(RandomTimeNotification)
 public class RandomTimeNotification: Notification {
-   @nonobjc public class func fetchRequest() -> NSFetchRequest<RandomTimeNotification> {
-       return NSFetchRequest<RandomTimeNotification>(entityName: "RandomTimeNotification")
-   }
-
-   @NSManaged public var startTime: Date
-   @NSManaged public var endTime: Date
-   
-   lazy var startTimeDefault: Date = {
-      var components = DateComponents()
-      components.hour = 9
-      components.minute = 0
-      return Cal.date(from: components) ?? Date()
-   }()
-   
-   lazy var endTimeDefault: Date = {
-      var components = DateComponents()
-      components.hour = 17
-      components.minute = 0
-      return Cal.date(from: components) ?? Date()
-   }()
-   
-   convenience init(myContext: NSManagedObjectContext, openAIDelegate: ChatGPTDelegate = OpenAI.shared, startTime: Date? = nil, endTime: Date? = nil) {
-      self.init(context: myContext)
-      super.moc = myContext
-      self.id = UUID()
-      self.unscheduledNotificationStrings = []
-      self.startTime = startTime ?? startTimeDefault
-      self.endTime = endTime ?? endTimeDefault
-   }
-   
-   override func nextDue() -> Date {
-      if let last = scheduledNotificationsArray.last {
-         let next = Cal.add(days: 1, to: last.date)
-         return next
-      } else {
-         let time = Cal.dateComponents([.hour, .minute], from: startTime)
-         let newDate = Cal.date(time: time, dayMonthYear: Date())
-         return newDate
-      }
-   }
-   
-   func getRandomTime() -> DateComponents {
-      let startTime = Cal.dateComponents([.hour, .minute], from: self.startTime)
-      let endTime = Cal.dateComponents([.hour, .minute], from: self.endTime)
-      
-      guard let startHour = startTime.hour,
-            let startMinute = startTime.minute,
-            let endHour = endTime.hour,
-            let endMinute = endTime.minute else {
-         fatalError("Unable to get hour and minutes for random notification")
-      }
-      
-      var startMinutes = startHour * 60 + startMinute
-      var endMinutes = endHour * 60 + endMinute
-      
-      // TODO: 1.0.9 fix this properly by not allowing it when saving
-      if startMinutes >= endMinutes {
-         let temp = startMinutes
-         startMinutes = endMinutes
-         endMinutes = temp
-      }
-      let randomTime = Int.random(in: startMinutes ..< endMinutes)
-      let randomHour = randomTime / 60
-      let randomMinute = randomTime % 60
-      
-      var components = DateComponents()
-      components.hour = randomHour
-      components.minute = randomMinute
-      return components
-   }
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<RandomTimeNotification> {
+        return NSFetchRequest<RandomTimeNotification>(entityName: "RandomTimeNotification")
+    }
+    
+    @NSManaged public var startTime: Date
+    @NSManaged public var endTime: Date
+    
+    lazy var startTimeDefault: Date = {
+        var components = DateComponents()
+        components.hour = 9
+        components.minute = 0
+        return Cal.date(from: components) ?? Date()
+    }()
+    
+    lazy var endTimeDefault: Date = {
+        var components = DateComponents()
+        components.hour = 17
+        components.minute = 0
+        return Cal.date(from: components) ?? Date()
+    }()
+    
+    convenience init(myContext: NSManagedObjectContext, openAIDelegate: ChatGPTDelegate = OpenAI.shared, startTime: Date? = nil, endTime: Date? = nil) {
+        self.init(context: myContext)
+        super.moc = myContext
+        self.id = UUID()
+        self.unscheduledNotificationStrings = []
+        self.startTime = startTime ?? startTimeDefault
+        self.endTime = endTime ?? endTimeDefault
+    }
+    
+    override func nextDue() -> Date {
+        if let last = scheduledNotificationsArray.last {
+            let next = Cal.add(days: 1, to: last.date)
+            return next
+        } else {
+            let time = Cal.dateComponents([.hour, .minute], from: startTime)
+            let newDate = Cal.date(time: time, dayMonthYear: Date())
+            return newDate
+        }
+    }
+    
+    func getRandomTime() -> DateComponents {
+        let startTime = Cal.dateComponents([.hour, .minute], from: self.startTime)
+        let endTime = Cal.dateComponents([.hour, .minute], from: self.endTime)
+        
+        guard let startHour = startTime.hour,
+              let startMinute = startTime.minute,
+              let endHour = endTime.hour,
+              let endMinute = endTime.minute else {
+            fatalError("Unable to get hour and minutes for random notification")
+        }
+        
+        var startMinutes = startHour * 60 + startMinute
+        var endMinutes = endHour * 60 + endMinute
+        
+        // TODO: 1.1.4 fix this properly by not allowing it when saving and showing an error
+        if startMinutes >= endMinutes {
+            let temp = startMinutes
+            startMinutes = endMinutes
+            endMinutes = temp
+        }
+        if startMinutes == endMinutes {
+            endMinutes += 1
+        }
+        
+        let randomTime = Int.random(in: startMinutes ..< endMinutes)
+        let randomHour = randomTime / 60
+        let randomMinute = randomTime % 60
+        
+        var components = DateComponents()
+        components.hour = randomHour
+        components.minute = randomMinute
+        return components
+    }
 }
