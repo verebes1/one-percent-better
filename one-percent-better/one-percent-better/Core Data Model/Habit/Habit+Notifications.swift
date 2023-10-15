d0f25758898ffe820c86a2c2183482f4029872a0
Jeremy Cook <jeremycook97@gmail.com>
2023-10-14 20:39:49 -0500

    Fixed crash which happened during deletion of habit

--- one-percent-better/one-percent-better/Core Data Model/Habit/Habit+Notifications.swift
+++ one-percent-better/one-percent-better/Core Data Model/Habit/Habit+Notifications.swift
@@ -9,86 +9,88 @@ import Foundation
 import UIKit
 
 extension Habit {
-   func addNotification(_ notification: Notification) {
-      self.addToNotifications(notification)
-      notificationManager.rebalanceHabitNotifications()
-   }
-   
-   func addNotifications(_ notifications: [Notification]) {
-      for notif in notifications {
-         self.addToNotifications(notif)
-      }
-      if !notifications.isEmpty {
-         notificationManager.rebalanceHabitNotifications()
-      }
-   }
-   
-   func removeNotifications(on date: Date) {
-      var rebalance = false
-      if notificationManager.rebalanceTask != nil {
-         rebalance = true
-         notificationManager.cancelRebalance()
-      }
-      
-      for notification in notificationsArray {
-         for scheduled in notification.scheduledNotificationsArray {
-            if Cal.isDate(date, inSameDayAs: scheduled.date), scheduled.isScheduled {
-               let identifier = "OnePercentBetter&\(scheduled.notification.id.uuidString)&\(scheduled.index)"
-               print("remove notification with id: \(identifier)")
-               UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [identifier])
-               scheduled.isScheduled = false
+    func addNotification(_ notification: Notification) {
+        self.addToNotifications(notification)
+        notificationManager.rebalanceHabitNotifications()
+    }
+    
+    func addNotifications(_ notifications: [Notification]) {
+        for notif in notifications {
+            self.addToNotifications(notif)
+        }
+        if !notifications.isEmpty {
+            notificationManager.rebalanceHabitNotifications()
+        }
+    }
+    
+    func removeNotifications(on date: Date) {
+        var rebalance = false
+        if notificationManager.rebalanceTask != nil {
+            rebalance = true
+            notificationManager.cancelRebalance()
+        }
+        
+        for notification in notificationsArray {
+            for scheduled in notification.scheduledNotificationsArray {
+                if Cal.isDate(date, inSameDayAs: scheduled.date), scheduled.isScheduled {
+                    let identifier = "OnePercentBetter&\(scheduled.notification.id.uuidString)&\(scheduled.index)"
+                    print("remove notification with id: \(identifier)")
+                    UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [identifier])
+                    scheduled.isScheduled = false
+                }
             }
-         }
-      }
-
-      removeDeliveredNotifications()
-      
-      if rebalance {
-         notificationManager.rebalanceHabitNotifications()
-      }
-   }
-   
-   func removeDeliveredNotifications() {
-      for habitNotif in self.notificationsArray {
-         let id = habitNotif.id
-         UNUserNotificationCenter.current().getDeliveredNotifications { [id] notifs in
-            for notif in notifs {
-               if notif.request.identifier.hasPrefix("OnePercentBetter&\(id)") {
-                  UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: [notif.request.identifier])
-               }
+        }
+        
+        removeDeliveredNotifications()
+        
+        if rebalance {
+            notificationManager.rebalanceHabitNotifications()
+        }
+    }
+    
+    func removeDeliveredNotifications() {
+        moc.performAndWait {
+            for habitNotif in self.notificationsArray {
+                let id = habitNotif.id
+                UNUserNotificationCenter.current().getDeliveredNotifications { [id] notifs in
+                    for notif in notifs {
+                        if notif.request.identifier.hasPrefix("OnePercentBetter&\(id)") {
+                            UNUserNotificationCenter.current().removeDeliveredNotifications(withIdentifiers: [notif.request.identifier])
+                        }
+                    }
+                }
             }
-         }
-      }
-   }
-   
-   func addNotificationsBack(on date: Date) {
-      var rebalance = false
-      if notificationManager.rebalanceTask != nil {
-         rebalance = true
-         notificationManager.cancelRebalance()
-      }
-      
-      for notification in notificationsArray {
-         for scheduled in notification.scheduledNotificationsArray {
-            if Cal.isDate(date, inSameDayAs: scheduled.date), !scheduled.isScheduled {
-               let identifier = "OnePercentBetter&\(scheduled.notification.id.uuidString)&\(scheduled.index)"
-               let dayComponents = Cal.dateComponents([.day, .month, .year, .hour, .minute], from: scheduled.date)
-               let trigger = UNCalendarNotificationTrigger(dateMatching: dayComponents, repeats: false)
-               let notifContent = notification.generateNotificationContent(message: scheduled.string)
-               let request = UNNotificationRequest(identifier: identifier, content: notifContent, trigger: trigger)
-               print("adding notification with id: \(identifier)")
-               UNUserNotificationCenter.current().add(request) { error in
-                  if let error = error {
-                     print("error adding notification back: \(error.localizedDescription)")
-                  }
-               }
-               scheduled.isScheduled = true
+        }
+    }
+    
+    func addNotificationsBack(on date: Date) {
+        var rebalance = false
+        if notificationManager.rebalanceTask != nil {
+            rebalance = true
+            notificationManager.cancelRebalance()
+        }
+        
+        for notification in notificationsArray {
+            for scheduled in notification.scheduledNotificationsArray {
+                if Cal.isDate(date, inSameDayAs: scheduled.date), !scheduled.isScheduled {
+                    let identifier = "OnePercentBetter&\(scheduled.notification.id.uuidString)&\(scheduled.index)"
+                    let dayComponents = Cal.dateComponents([.day, .month, .year, .hour, .minute], from: scheduled.date)
+                    let trigger = UNCalendarNotificationTrigger(dateMatching: dayComponents, repeats: false)
+                    let notifContent = notification.generateNotificationContent(message: scheduled.string)
+                    let request = UNNotificationRequest(identifier: identifier, content: notifContent, trigger: trigger)
+                    print("adding notification with id: \(identifier)")
+                    UNUserNotificationCenter.current().add(request) { error in
+                        if let error = error {
+                            print("error adding notification back: \(error.localizedDescription)")
+                        }
+                    }
+                    scheduled.isScheduled = true
+                }
             }
-         }
-      }
-      
-      if rebalance {
-         notificationManager.rebalanceHabitNotifications()
-      }
-   }
+        }
+        
+        if rebalance {
+            notificationManager.rebalanceHabitNotifications()
+        }
+    }
 }
