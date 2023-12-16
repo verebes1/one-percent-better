88d166f262ab5c730a4bf8bc2318cc58388238f4
Jeremy Cook <jeremycook97@gmail.com>
2023-12-16 13:48:40 +0100

    dev device

--- one-percent-better/one-percent-better/Settings Tab Views/DeveloperController.swift
+++ one-percent-better/one-percent-better/Settings Tab Views/DeveloperController.swift
@@ -10,7 +10,10 @@ import UIKit
 class DeveloperController {
     static let shared = DeveloperController()
     
-    private var allowList: Set<String> = ["D8BDA928-DD89-4A26-84FB-B2770C0D4B52"]
+    private var allowList: Set<String> = [
+        "D8BDA928-DD89-4A26-84FB-B2770C0D4B52",
+        "84B9DA5C-F47E-4D2F-AFAB-5EDDD8122270"
+    ]
     
     public var isDeveloper: Bool {
         guard let udid = UIDevice.current.identifierForVendor?.uuidString else {
