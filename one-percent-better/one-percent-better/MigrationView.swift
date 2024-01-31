134a8cd122c4bac2ead1456edb6dea8e654ddbd5
Jeremy Cook <jeremycook97@gmail.com>
2024-01-30 19:04:50 -0600

    Start of migration

---
+++ one-percent-better/one-percent-better/MigrationView.swift
@@ -0,0 +1,26 @@
+//
+//  MigrationView.swift
+//  one-percent-better
+//
+//  Created by Jeremy Cook on 1/29/24.
+//
+
+import SwiftUI
+
+struct MigrationView: View {
+    var body: some View {
+        VStack {
+            Text("Migration to New App")
+            Button("Export Data") {
+                //
+            }
+            Button("Install New App") {
+                //
+            }
+        }
+    }
+}
+
+#Preview {
+    MigrationView()
+}
